631db3bb0addb3c8deb32262a48eb561be412d3d
kennansalisbury <kennansalisbury@gmail.com>
2020-05-30 13:50:50 -0400

    saving

--- js/script.js
+++ js/script.js
@@ -1,1 +1,30 @@
 console.log('Hello, front end');
+
+// input field
+// submit button
+// clear button
+
+// User can put in a value into the text field
+// User click on a button
+// Update converted temperature
+// Clear button to reset the text field
+
+
+let input = document.getElementById('input')
+console.log(input)
+
+let submitBtn = document.getElementById('submit')
+let clearBtn = document.getElementById('clear')
+let display = document.querySelector('.display')
+
+submitBtn.addEventListener('click', (e) => {
+    e.preventDefault()
+    display.innerHTML = input.value
+})
+
+clearBtn.addEventListener('click', (e) => {
+    e.preventDefault()
+    display.innerHTML = ' '
+})
+
+display.innerHTML = 'test content'
